68a0c00b0f51bdb5f12a4e87f44838042f1e7ab5
Bashtan <Andrii>
2025-08-30 10:29:29 -0500

    update

--- src/api/eventsAPI.js
+++ src/api/eventsAPI.js
@@ -13,21 +13,21 @@ export async function getEventsByDate(dateISO) {
   ];
 }
 
-export async function getEventsByRange(fromISO, toISO) {
-//   const r = await fetch(`${BASE}/events?from=${fromISO}&to=${toISO}`);
-//   if (!r.ok) throw new Error("Failed to load month events");
-//   return r.json();
-    return [
-  { "id": 1, "title": "Youth", "eventDate": "2025-07-02" },
-  { "id": 2, "title": "Sunday Service", "eventDate": "2025-07-02" },
-  { "id": 3, "title": "Prayer", "eventDate": "2025-08-15" }
-];
-}
+// export async function getEventsByRange(fromISO, toISO) {
+// //   const r = await fetch(`${BASE}/events?from=${fromISO}&to=${toISO}`);
+// //   if (!r.ok) throw new Error("Failed to load month events");
+// //   return r.json();
+//     return [
+//   { "id": 1, "title": "Youth", "eventDate": "2025-07-02" },
+//   { "id": 2, "title": "Sunday Service", "eventDate": "2025-07-02" },
+//   { "id": 3, "title": "Prayer", "eventDate": "2025-08-15" }
+// ];
+// }
 
-export function monthRange(anchorISO) {
-  const d = dayjs(anchorISO);
-  return {
-    from: d.startOf("month").format("YYYY-MM-DD"),
-    to: d.endOf("month").format("YYYY-MM-DD"),
-  };
-}
+// export function monthRange(anchorISO) {
+//   const d = dayjs(anchorISO);
+//   return {
+//     from: d.startOf("month").format("YYYY-MM-DD"),
+//     to: d.endOf("month").format("YYYY-MM-DD"),
+//   };
+// }
