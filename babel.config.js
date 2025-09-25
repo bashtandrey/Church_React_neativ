7cd84d79f0bcccc3949c4c6905fe945cb08f2b47
Bashtan <Andrii>
2025-09-25 13:52:19 -0500

    clean

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
-        'module-resolver',
+        "module-resolver",
         {
-          root: ['./src'],
-          alias: { '@': './src' },
+          alias: {
+            "@": "./src",
+          },
         },
       ],
-      'react-native-reanimated/plugin',
+      "react-native-reanimated/plugin",
     ],
   };
 };
